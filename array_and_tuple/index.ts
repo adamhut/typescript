cdc1780eff697fae8a22ab8f81a19d74c33201e0
aHuang <adamhut@gmail.com>
2021-06-03 08:00:15 +0800

    array  and tuple

---
+++ array_and_tuple/index.ts
@@ -0,0 +1,16 @@
+//Array 
+
+const LaracastsTeam: string[] =[
+    'Jeffery Way',
+    'Tudor Andrin',
+    'Andre Madarang'
+];
+
+const listOfNumber: number[] =[1,2,3,4]
+
+const numbers: Array<number> = [ 2, 3, 4]
+
+const stringAndNumber: (string|number)[] = ['1',2,4,'adam'];
+
+//tuples
+const tuple:[string,number] =['adam',1234]
